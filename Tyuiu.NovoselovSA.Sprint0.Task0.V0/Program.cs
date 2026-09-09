8b84f253112e029d77b02815a80c427cf8f5b781
Степан Новосёлов <Tyuiu.NovoselovSA@outlook.com>
2026-09-10 01:02:11 +0500

    task0

--- Tyuiu.NovoselovSA.Sprint0.Task0.V0/Program.cs
+++ Tyuiu.NovoselovSA.Sprint0.Task0.V0/Program.cs
@@ -10,11 +10,6 @@ namespace Tyuiu.NovoselovSA.Sprint0.Task0.V0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
-            string name = "Степан";
-            int age = 17;
-            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + "лет!");
-            Console.ReadKey();
         }
     }
 }
